703e8400decaa5f5dfcaea488d03d17d18837d9e
raulestevam <estevamraul07@gmail.com>
2026-02-25 21:42:55 -0300

    como usar if else

---
+++ Aula_03_typescript/src/condicionais.ts
@@ -0,0 +1,10 @@
+let idade: number=18;
+
+if(idade < 12)
+    console.log("Crianca");
+else if (idade < 18)
+    console.log("Adolescente");
+else if (idade <60)
+    console.log("Adulto");
+else 
+    console.log("Idoso");
