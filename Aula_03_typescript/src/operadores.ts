a9c4a22034bbc9f37032f35f805893a921940d84
raulestevam <estevamraul07@gmail.com>
2026-02-25 21:42:47 -0300

    como fazer operacaoes simples

---
+++ Aula_03_typescript/src/operadores.ts
@@ -0,0 +1,12 @@
+let a:number = 10;
+let b:number = 5;
+
+//operações
+console.log("Soma:", a+b);
+console.log("subtracao:", a-b);
+console.log("multplicacao:", a*b);
+console.log("divisao:", a/b);
+console.log("modulo:", a%b);
+console.log("exponenciacao:", a**b);
+console.log("igualdade:", a==b);
+console.log("igualdade estrita:", a===b);
