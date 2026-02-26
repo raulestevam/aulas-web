f1afd0c1782f5c7c0ddd7328b9f7804615516fa3
raulestevam <estevamraul07@gmail.com>
2026-02-25 22:09:24 -0300

    como fazer loops

---
+++ Aula_03_typescript/src/loop.ts
@@ -0,0 +1,21 @@
+let i:number;
+
+for(i=0; i<5; i++)
+    console.log(`Iteracao: ${i}`);
+
+let contador= 0
+while(contador <5)
+    console.log(`Iteracao: ${contador}`);
+
+do{
+
+}while(contador<3);
+
+// const numeros = [1, 2, 3, 4, 5];
+// for (const num of numeros)
+//     console.log("Numeros:", num);
+
+// const pessoa = {nome:"Raul", idade:18, cidade:"Boituva"};
+// for(const chave in pessoa)
+//     console.log()
+
