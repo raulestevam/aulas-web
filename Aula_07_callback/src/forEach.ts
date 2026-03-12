009c368c02ea3133cdb5482489c7167e0d45c28b
raulestevam <estevamraul07@gmail.com>
2026-03-11 22:11:17 -0300

    faz busca com condição

---
+++ Aula_07_callback/src/forEach.ts
@@ -0,0 +1,5 @@
+const frutas: string[] = ['banana', 'uva', 'maca'];
+const numbers: number[] = [1, 2, 3, 4, 5];
+let contador: number = 0;
+//percorre itens dentro de um vetor
+numbers.forEach(item=> item * 2);
