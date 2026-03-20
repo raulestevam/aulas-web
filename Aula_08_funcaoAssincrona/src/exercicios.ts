5e24c30bab2a176c2aabe288323c25eb5c04f043
estevamraul07@gmail.com <estevamraul07@gmail.com>
2026-03-20 20:28:52 -0300

    exercicios corrgidos

--- Aula_08_funcaoAssincrona/src/exercicios.ts
+++ Aula_08_funcaoAssincrona/src/exercicios.ts
@@ -11,10 +11,20 @@ function imprimirTarefa(descricao: string, indice: number, totalTarefas: number)
     console.log(`Progresso: ${indice + 1}/${totalTarefas}`);
 }
 
-function executarTarefas(
-    tarefas: Tarefa[],
-    callback:(descricao: string, prioridade: number, concluida: boolean)=>void,
-    time: number
-):void{
+function executarTarefas(tarefas: Tarefa[], callback:(descricao: string, indice: number, totalTarefas: number)=>void, time: number = 1000):void{
     tarefas.sort((a, b)=>a.prioridade - b.prioridade);
+    let contador = 0;
+    const interval = setInterval(()=>{
+        if(contador >= tarefas.length || tarefas[contador]?.descricao === "Cancelar")
+            clearInterval(interval);
+        else{
+            const tarefa = tarefas[contador]
+            if(tarefa){
+                tarefa.concluida = true;
+                callback(tarefa.descricao, contador, tarefas.length);
+                contador++;
+            }
+        }
+    }, time);
 }
+executarTarefas(tarefas, imprimirTarefa, 1500);
