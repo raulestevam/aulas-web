8aa667a9a03355f283f88cbe1273c726d2cdac48
raulestevam <estevamraul07@gmail.com>
2026-03-27 20:18:15 -0300

    class aluno com retorno dos dados

---
+++ Aula_09_classes/src/Aluno.ts
@@ -0,0 +1,16 @@
+export class Aluno{
+    nome: string
+    idade: number
+    curso: string
+    matricula: string
+
+    constructor(nome: string, idade: number, curso: string, matricula: string){
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+        this.matricula = matricula;
+    }
+    imprimir():void{
+        console.log(`Nome: ${this.nome}\nIdade: ${this.idade}\nCurso: ${this.curso}\nMatricula: ${this.matricula}`);
+    }
+} 
