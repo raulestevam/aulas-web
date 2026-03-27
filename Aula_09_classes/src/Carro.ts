4a00c564aa5a8378b479cc0443264ecd9203c04f
raulestevam <estevamraul07@gmail.com>
2026-03-27 20:34:01 -0300

    finalizando classe carro

---
+++ Aula_09_classes/src/Carro.ts
@@ -0,0 +1,10 @@
+export class Carro{
+    marca: string;
+    modelo: string;
+    ano: number;
+    constructor(marca: string, modelo: string, ano: number){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
